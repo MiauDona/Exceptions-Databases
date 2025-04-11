8e6b38124f61f7cda3e00e87d4d8dff2a0ac5711
miaudona <donanferyt@gmail.com>
2025-04-11 14:28:38 +0200

    Small change

--- src/main/java/miau/dona/db/Test.java
+++ src/main/java/miau/dona/db/Test.java
@@ -3,7 +3,6 @@ package miau.dona.db;
 import java.sql.*;
 
 public class Test {
-
     public static void main(String[] args) {
         Connection con = null;
         PreparedStatement preparedStatement = null;
@@ -11,26 +10,27 @@ public class Test {
 
         try {
             System.out.println("---------------------------Empieza");
-            con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/miaudonatest", "admindona", "miaudona");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/miaudonatest", "admindona", "miaudona");
 
             preparedStatement = con.prepareStatement("select id, nombre from test1");
-
             rs = preparedStatement.executeQuery();
 
             while (rs.next()) {
                 System.out.println(rs.getString(1));
             }
+
         } catch (SQLException e) {
             System.out.println("---------------nofunicona");
             System.out.println(e);
-            throw new RuntimeException(e);
 
         } finally {
             System.out.println("---------------------Termina");
+
             try {
                 con.close();
                 rs.close();
                 preparedStatement.close();
+
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
